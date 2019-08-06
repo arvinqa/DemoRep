01694dbc6e2a4cdf88a654f8c8622261c015601e
Arvin Christopher Sebolino <arvin.sebolino@cybilltek.local>
2019-08-06 15:32:35 +0800

    added search , edited login

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,4 +5,8 @@ public class LoginPage {
 	public void test(){
 		System.out.println("test Login Page");
 	}
+	
+	public void test2(){
+		System.out.println("test Login2 Page");
+	}
 }
---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class SearchPage {
+
+	public void test(){
+		System.out.println("test Search Page");
+	}
+	
+}
